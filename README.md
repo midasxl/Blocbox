95186882cfbc541b6c460d3c6dfd051cd587db54
Midasxl <sparkhw@gmail.com>
2016-02-17 17:41:29 -0500

    First commit for gh-pages

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blocbox
+
+## Assignment 27 is Complete!
---
+++ js/accountdrop.js
@@ -0,0 +1,96 @@
+/*
+ * Dropit v1.1.0
+ * http://dev7studios.com/dropit
+ *
+ * Copyright 2012, Dev7studios
+ * Free to use and abuse under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+;(function($) {
+
+    "use strict";
+	$.fn.accountdrop = function(method) {
+
+        var methods = {
+
+            init : function(options) {
+                this.accountdrop.settings = $.extend({}, this.accountdrop.defaults, options);
+                return this.each(function() {
+                    var $el = $(this), el = this, settings = $.fn.accountdrop.settings;
+
+                    // Hide initial submenus
+                    $el.addClass('acctdropit')
+                    .find('>'+ settings.triggerParentEl +':has('+ settings.submenuEl +')').addClass('acctdropit-trigger')
+                    .find(settings.submenuEl).addClass('acctdropit-submenu').hide();
+
+                    // Open on click
+                    $el.off(settings.action).on(settings.action, settings.triggerParentEl +':has('+ settings.submenuEl +') > '+ settings.triggerEl +'', function(){
+                        // Close click menu's if clicked again
+                        if(settings.action === 'click' && $(this).parents(settings.triggerParentEl).hasClass('acctdropit-open')){
+                            settings.beforeHide.call(this);
+                            $(this).parents(settings.triggerParentEl).removeClass('acctdropit-open').find(settings.submenuEl).hide();
+                            settings.afterHide.call(this);
+                            return false;
+                        }
+
+                        // Hide open menus
+                        settings.beforeHide.call(this);
+                        $('.acctdropit-open').removeClass('acctdropit-open').find('.acctdropit-submenu').hide();
+                        settings.afterHide.call(this);
+
+                        // Open this menu
+                        settings.beforeShow.call(this);
+                        $(this).parents(settings.triggerParentEl).addClass('acctdropit-open').find(settings.submenuEl).show();
+                        settings.afterShow.call(this);
+
+                        return false;
+                    });
+
+                    // Close if outside click
+                    $(document).on('click', function(){
+                        settings.beforeHide.call(this);
+                        $('.acctdropit-open').removeClass('acctdropit-open').find('.acctdropit-submenu').hide();
+                        settings.afterHide.call(this);
+                    });
+
+                    // If hover
+                    if(settings.action === 'mouseenter'){
+                        $el.on('mouseleave', '.acctdropit-open', function(){
+                            settings.beforeHide.call(this);
+                            $(this).removeClass('acctdropit-open').find(settings.submenuEl).hide();
+                            settings.afterHide.call(this);
+                        });
+                    }
+
+                    settings.afterLoad.call(this);
+                });
+            }
+
+        };
+
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error( 'Method "' +  method + '" does not exist in accountdrop plugin!');
+        }
+
+    };
+
+    $.fn.accountdrop.defaults = {
+        action: 'click', // The open action for the trigger
+        submenuEl: 'ul', // The submenu element
+        triggerEl: 'a', // The trigger element
+        triggerParentEl: 'li', // The trigger parent element
+        afterLoad: function(){}, // Triggers when plugin has loaded
+        beforeShow: function(){}, // Triggers before submenu is shown
+        afterShow: function(){}, // Triggers after submenu is shown
+        beforeHide: function(){}, // Triggers before submenu is hidden
+        afterHide: function(){} // Triggers before submenu is hidden
+    };
+
+    $.fn.accountdrop.settings = {};
+
+})(jQuery);
